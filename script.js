58ecd7bf00bc6a27cb1162a8e0bd058548aac0e1
Robin Le Floch <77750420+nonfinito@users.noreply.github.com>
2024-05-31 09:48:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let taxiFareApiUrl = 'https://taxifare-nonfinito-6n3gdpz53a-ew.a.run.app/predict
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
-  taxiFareApiUrl = 'https://taxifare-nonfinito-6n3gdpz53a-ew.a.run.app/predict';
+  taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
